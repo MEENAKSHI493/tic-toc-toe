6fa002f397b37c7c584b1e120eba313de2127d11
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-25 10:34:35 +0530

    Add player choose o or x

--- src/com/bridgelabz/tictoctoegame.java
+++ src/com/bridgelabz/tictoctoegame.java
@@ -1,19 +1,35 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
-public class tictoctoegame {
-
-		
+public class tictoctoegame {		
 private static char[] element;
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Tic Tac Toe Game....!!!");
+		System.out.println("Welcome to Tic Tac Toe Game");
 		board();
+		userinput();
 	}
-	
+   
 	private static void board() {
 		element = new char[10];
 		for(int i=1; i<10; i++) {
 			element[i]='.';
 		}
+	}
+	private static void userinput() {
+		 Scanner in=new Scanner(System.in);
+		  
+		  //Mark Selection
+		  System.out.println(" Select Your Marker (O or X): ");
+		  
+		 char usermark=in.next().charAt(0);
+		  char computermark;
+		  if (usermark=='X') {
+		  computermark='O';
+		  System.out.println("User mark is :" +usermark);}
+		  
+		  else
+		   computermark='X';
+		  System.out.println("User mark is :" +usermark);
 	}
 }
