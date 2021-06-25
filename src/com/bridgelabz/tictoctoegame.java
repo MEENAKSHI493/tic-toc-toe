4144803d0ff8ae7dc753fbce39c5e12a2a5e6d52
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-25 09:10:55 +0530

    tic toc game

--- src/com/bridgelabz/tictoctoegame.java
+++ src/com/bridgelabz/tictoctoegame.java
@@ -2,9 +2,20 @@ package com.bridgelabz;
 
 public class tictoctoegame {
 
+		
+private static char[] element;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Tic Tac Toe Game....!!!");
+		board();
 	}
-
+	
+   
+	
+	private static void board() {
+		element = new char[10];
+		for(int i=1; i<10; i++) {
+			element[i]='.';
+		}
+	}
 }
